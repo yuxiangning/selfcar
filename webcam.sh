96e1c99e4d65b9eb51c37318f3ed1b6fcf335b4e
Xiangning Yu <yuxiangning@gmail.com>
2017-12-16 06:21:02 +0000

    add a simple web camera script

---
+++ webcam.sh
@@ -0,0 +1,6 @@
+raspistill --nopreview -w 320 -h 240 -q 5 -o /tmp/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &
+
+export LD_LIBRARY_PATH=/home/pi/src/mjpg-streamer-code-182/mjpg-streamer
+cd ./mjpg-streamer-code-182/mjpg-streamer
+
+./mjpg_streamer -i "input_file.so -f /tmp/ -n pic.jpg" -o "output_http.so -w ./www"
