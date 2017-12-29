4f8dfc49fcf0f496d676c592587708ed8c08c373
Xiangning Yu <yuxiangning@gmail.com>
2017-12-29 03:15:09 +0000

    add some display info

--- car.py
+++ car.py
@@ -7,6 +7,19 @@ import curses
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 
+# begin with a simple softmax linear mode
+class Model:
+	def __init__(self, w_dim, b_dim):
+		# self.W = np.ndarray(shape=[1, w_dim], dtype=float)
+		# self.b = np.ndarray(shape=[b_dim, 1], dtype=float)
+		self.W = np.matrix([[1,2,3,4]])
+		self.b = np.matrix([[5,5,5,5,5]])
+		self.b = self.b.reshape(5,1)
+
+	def Calculate(self, x):
+		y = np.add(np.dot(self.W, x), self.b)
+		print(y)
+
 class Car:
 	def __init__(self):
 		motor_p_pin = 14
@@ -84,12 +97,12 @@ class Car:
 			self.UpdateThrottle(-5)
 		return True
 
-	def Run(self, recording):
+	def Run(self, recording, use_keyboard):
 		# capture dimention
 		fps = 30
 		width = 160
 		height = 128
-		if recording == True:
+		if use_keyboard == True:
 			self.BeginKeyBoardInput()
 		camera = PiCamera()
 		camera.resolution = (width, height)
@@ -97,11 +110,16 @@ class Car:
 		rawCapture = PiRGBArray(camera, size = (width, height))
 		seq = 0
 		begin = time.time()
-		fd = open('./data/steer.csv', 'a')
+		if recording == True:
+			fd = open('./data/steer.csv', 'a')
 
 		for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-			if recording and self.HandleKeyBoardIntput() == False:
+			if use_keyboard and self.HandleKeyBoardIntput() == False:
 				break
+
+			str = "CAPTURE: {} STEER: {} THROTTLE: {} FPS: {}".format(seq, self.steer_angle, self.throttle, fps)
+			self.stdscr.addstr(0, 0, str, curses.A_REVERSE)
+			self.stdscr.refresh()
 
 			image = frame.array
 			if recording == True:
@@ -111,22 +129,24 @@ class Car:
 				rec = name + ',' + str(self.steer_angle) + '\n'
 				fd.write(rec)
 
+			# XXX how to leverage four cores on raspberry pi???
 			image = self.PreProcess(image, width, height)
 			rawCapture.truncate(0)
 
 			seq += 1
 			if not (seq % 100):
 				end = time.time()
-				print("fps %d\n", seq/(end - begin))
+				fps = seq/(end - begin)
 
-		fd.close()
+		if recording:
+			fd.close()
 
 	def Halt(self):
 		self.s_pwm.stop()
 		self.d_pwm.stop()
 		GPIO.cleanup()
 
-		if recording:
+		if use_keyboard:
 			self.EndKeyBoardInput()
 
 	def TopView(self, img, width, height):
@@ -146,9 +166,10 @@ class Car:
 		return img
 
 # global params
-recording = True
+use_keyboard = True
+recording = False
 try:
 	c = Car()
-	c.Run(recording)
+	c.Run(recording, use_keyboard)
 finally:
 	c.Halt()
